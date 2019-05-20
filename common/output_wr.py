c62e162f7bc698dca75ffb80f63ec306b0edde86
Anouar Ben Saad <bensaad.tig@gmail.com>
2019-05-20 05:18:28 +0100

    added, write logs file method

---
+++ common/output_wr.py
@@ -0,0 +1,13 @@
+
+import os
+import sys
+
+def writelogs(data, data_name, output_dir):
+    """Write the results."""
+    for data, data_name in zip(data, data_name):
+        if data:
+            filepath = output_dir + '/' + data_name + '.txt'
+            with open(filepath, 'w+') as out_file:
+                joined = '\n'.join(data)
+                out_file.write(str(joined.encode('utf-8').decode('utf-8')))
+                out_file.write('\n')
