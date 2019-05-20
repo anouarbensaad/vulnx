d54e2f45a509becde52e536b4ac501c037fb76ec
Anouar Ben Saad <bensaad.tig@gmail.com>
2019-05-20 02:31:15 +0100

    adding dorks list

---
+++ common/dorks_list.py
@@ -0,0 +1,21 @@
+from common.colors import R,W
+def dorkslist():
+    print ("""
+---------%sWordPress%s---------     ---------%sJoomla%s---------     ---------%sDrupal%s---------     ---------%sPrestashop%s---------
+-  blaze                        comjce                  
+-  catpro                                               
+-  cherry                                               
+-  dm                                                   
+-  fromcraft                                            
+-  synoptic                                             
+-  shop                                                 
+-  revslider                                            
+-  adsmanager                                           
+-  inboundiomarketing                                   
+-  wysija
+-  powerzoomer
+-  showbiz
+-  jobmanager
+-  injection
+
+    """ % (R,W,R,W,R,W,R,W))
