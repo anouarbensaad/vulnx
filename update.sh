b26bbf1477ec96c8e59c63ee66b0afedacc1a8cb
Anouar Ben Saad <bensaad.tig@gmail.com>
2019-05-25 04:11:04 +0100

    Updating Method.

--- update.sh
+++ update.sh
@@ -2,29 +2,51 @@ red="\e[0;31m"
 green="\e[0;32m"
 off="\e[0m"
 function banner(){
-    echo "nothing"
+    echo -e "===== VULNX INSTALL ====="
 }
-function update() {
-echo -e "$red [$green+$red]$off Cleaning Up Old Directories ...";
+function termuxOS() {
+  echo -e "$red [$green+$red]$Cleaning Up Old Directories ...";
+  rm -r "/data/data/com.termux/files/usr/share/vulnx"
+  echo -e "$red [$green+$red]$off Installing ...";
+  git clone https://github.com/anouarbensaad/vulnx "/data/data/com.termux/files/usr/share/vulnx";
+  rm -r "/data/data/com.termux/files/usr/share/vulnx/config"
+  if [ -d "/data/data/com.termux/files/usr/share/vulnx" ] ;
+  then
+    echo -e "$red [$green+$red]$off Tool Successfully Updated And Will Start In 5s!";
+    echo -e "$red [$green+$red]$off You can execute tool by typing vulnx"
+    sleep 5;
+    vulnx
+  else
+    echo -e "$red [$green✘$red]$off Tool Cannot Be Installed On Your System! Use It As Portable !";
+    exit
+  fi 
+}
+
+function debianOS() {
+  echo -e "$red [$green+$red]$off Cleaning Up Old Directories ...";
   sudo rm -r "/usr/share/vulnx"
   echo -e "$red [$green+$red]$off Installing ...";
- sudo git clone https://github.com/anouarbensaad/vulnx "/usr/share/vulnx";
-  sudo rm -r "/usr/share/vulnx/conf"
+  sudo git clone https://github.com/Moham3dRiahi/vulnx "/usr/share/vulnx";
+  sudo rm -r "/usr/share/vulnx/config"
 
-if [ -d "/usr/share/vulnx" ] ;
-then
-echo -e "$red [$green+$red]$off Tool Successfully Updated And Will Start In 5s!";
-echo -e "$red [$green+$red]$off You can execute tool by typing vulnx";
-sleep 5;
-vulnx
-else
-echo -e "$red [$green✘$red]$off Tool Cannot Be Installed On Your System! Use It As Portable !";
+  if [ -d "/usr/share/vulnx" ] ;
+  then
+    echo -e "$red [$green+$red]$off Tool Successfully Updated And Will Start In 5s!";
+    echo -e "$red [$green+$red]$off You can execute tool by typing vulnx";
+    sleep 5;
+    vulnx
+  else
+    echo -e "$red [$green✘$red]$off Tool Cannot Be Installed On Your System! Use It As Portable !";
     exit
-fi 
+  fi 
 }
 
-if [ -d "/usr/bin/" ];then
+if [ -d "/data/data/com.termux/files/usr/" ]; then
 banner
 echo -e "$red [$green+$red]$off vulnx Will Be Installed In Your System";
-update
+termux
+elif [ -d "/usr/bin/" ];then
+banner
+echo -e "$red [$green+$red]$off vulnx Will Be Installed In Your System";
+linux
 fi
