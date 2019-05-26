c5e3753d593fbc203d3b9013a4a8b2c396b63a95
Anouar Ben Saad <bensaad.tig@gmail.com>
2019-05-26 07:35:10 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-red="\e[0;31m"
-green="\e[0;32m"
-off="\e[0m"
+red   = "\e[0;31m"
+green = "\e[0;32m"
+off   = "\e[0m"
 function banner(){
     echo -e "===== VULNX INSTALL ====="
 }
@@ -10,8 +10,7 @@ function termuxOS() {
   echo -e "$red [$green+$red]$off Installing ...";
   git clone https://github.com/anouarbensaad/vulnx "/data/data/com.termux/files/usr/share/vulnx";
   rm -r "/data/data/com.termux/files/usr/share/vulnx/config"
-  if [ -d "/data/data/com.termux/files/usr/share/vulnx" ] ;
-  then
+  if [[ -d "/data/data/com.termux/files/usr/share/vulnx" ]]; then
     echo -e "$red [$green+$red]$off Tool Successfully Updated And Will Start In 5s!";
     echo -e "$red [$green+$red]$off You can execute tool by typing vulnx"
     sleep 5;
@@ -26,11 +25,9 @@ function debianOS() {
   echo -e "$red [$green+$red]$off Cleaning Up Old Directories ...";
   sudo rm -r "/usr/share/vulnx"
   echo -e "$red [$green+$red]$off Installing ...";
-  sudo git clone https://github.com/Moham3dRiahi/vulnx "/usr/share/vulnx";
+  sudo git clone https://github.com/anouarbensaad/vulnx "/usr/share/vulnx";
   sudo rm -r "/usr/share/vulnx/config"
-
-  if [ -d "/usr/share/vulnx" ] ;
-  then
+  if [[ -d "/usr/share/vulnx" ]]; then
     echo -e "$red [$green+$red]$off Tool Successfully Updated And Will Start In 5s!";
     echo -e "$red [$green+$red]$off You can execute tool by typing vulnx";
     sleep 5;
@@ -40,8 +37,7 @@ function debianOS() {
     exit
   fi 
 }
-
-if [ -d "/data/data/com.termux/files/usr/" ]; then
+if [[ -d "/data/data/com.termux/files/usr/" ]]; then
 banner
 echo -e "$red [$green+$red]$off vulnx Will Be Installed In Your System";
 termuxOS
