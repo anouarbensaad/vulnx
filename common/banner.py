2fb3ed5e09c046f5e362e9f9ca24c345f7b568d4
Anouar Ben Saad <bensaad.tig@gmail.com>
2019-05-31 00:13:12 +0100

    ReFix Banner #33

--- common/banner.py
+++ common/banner.py
@@ -1,43 +1,8 @@
 import sys
-from common.colors import bannerblue ,bannerblue2 ,W ,Y ,end
+from common.colors import bannerblue , bannerblue2 ,W ,Y ,R,end
 
 def banner():
-  if sys.platform.lower().startswith(('os', 'win', 'darwin', 'ios')):
-    # Colors shouldn't be displayed on Mac and Windows
-      print("""%s
-                                                      
-                    .:.        .:,                    
-                   xM;           XK.                  
-                  dx'            .lO.                 
-                 do                ,0.                
-             .c.lN'      ,  '.     .k0.:'             
-              xMMk;d;''cOM0kWXl,',locMMX.             
-              .NMK.   :WMMMMMMMx    dMMc              
-               lMMO  lWMMMMMMMMMO. lMMO               
-                cWMxxMMMMMMMMMMMMKlWMk                
-                 .xWMMMMMMMMMMMMMMM0,%s                 
-                   .,OMd,,,;0MMMO,.                   
-             .l0O.%sVXVX%sOX.%sVXVX%s0MO%sVXVX%s.0Kd,             
-            lWMMO0%sVXVX0%sOX.%sVXVX%sl%sVXVX%s.VXNMMO            
-           .MMX;.N0%sVXVX0%s0X.%sVXVXVX0%s.0M:.OMMl           
-          .OXc  ,MMO%sVXVX0%sVX%s .VXVX0%s0MMo  ,0X'          
-          0x.  :XMMMk%sVXVX.%sXO.%sVXVX%sdMMMWo.  :X'         
-         .d  'NMMMMMMk%sVXVX%s..%sVXVX0%s.XMMMMWl  ;c         
-            'NNoMMMMMMx%sVXVXVXVXVX0.%sXMMk0Mc            
-           .NMx OMMMMMMd%sVXVXVX%sl%sVXVX%s.NW.;MMc           
-          :NMMd .NMMMMMMd%sVXVX%sdMd,,,,oc ;MMWx          
-          .0MN,  'XMMMMMMo%sVX%soMMMMMMWl   0MW,          
-           .0.    .xWMMMMM:lMMMMMM0,     kc           
-            ,O.     .:dOKXXXNKOxc.      do            
-             '0c        -VulnX-       ,Ol             
-               ;.                     :.   
-
-    %s# Coded By Anouar Ben Saad -%s @anouarbensaad      
-                                           %s""" )
-  
-  
-  else:
-    print("""%s
+  print("""%s
                                                       
                     .:.        .:,                    
                    xM;           XK.                  
@@ -67,17 +32,17 @@ def banner():
 
     %s# Coded By Anouar Ben Saad -%s @anouarbensaad      
                                            %s""" 
-    % 
-    (bannerblue,bannerblue2,
-    W,bannerblue2,W,bannerblue2,W,bannerblue2,
-    W,bannerblue2,W,bannerblue2,W,bannerblue2,
-    W,bannerblue2,W,bannerblue2, 
-    W,bannerblue2,W,bannerblue2,
-    W,bannerblue2,W,bannerblue2,
-    W,bannerblue2,W,bannerblue2,
-    W,bannerblue2,
-    W,bannerblue2,W,bannerblue2,
-    W,bannerblue2,
-    W,bannerblue2,
-    W,Y,end
-    ))
+% 
+(bannerblue,bannerblue2,
+W,bannerblue2,W,bannerblue2,W,bannerblue2,
+W,bannerblue2,W,bannerblue2,W,bannerblue2,
+W,bannerblue2,W,bannerblue2, 
+W,bannerblue2,W,bannerblue2,
+W,bannerblue2,W,bannerblue2,
+W,bannerblue2,W,bannerblue2,
+W,bannerblue2,
+W,bannerblue2,W,bannerblue2,
+W,bannerblue2,
+W,bannerblue2,
+W,Y,end
+))
